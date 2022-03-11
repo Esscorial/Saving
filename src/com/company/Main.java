c840fc4f2a8c3d4586f51b7bbd20ba2848606021
Esscorial <esscorial@yandex.ru>
2022-03-11 22:31:07 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameProgress game1 = new GameProgress(15, 300, 3, 105.62);
+        GameProgress game2 = new GameProgress(36, 100, 10, 508.65);
+        GameProgress game3 = new GameProgress(75, 600, 15, 948.23);
+        saveGame("D://Gamess//savegames//game1.dat", game1);
+        saveGame("D://Gamess//savegames//game2.dat", game2);
+        saveGame("D://Gamess//savegames//game3.dat", game3);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("D://Gamess//savegames//game1.dat");
+        arrayList.add("D://Gamess//savegames//game2.dat");
+        arrayList.add("D://Gamess//savegames//game3.dat");
+        zipFiles("D://Gamess//savegames//zip.zip", arrayList);
+        File game1Dat = new File("D://Gamess//savegames//game1.dat");
+        File game2Dat = new File("D://Gamess//savegames//game2.dat");
+        File game3Dat = new File("D://Gamess//savegames//game3.dat");
+        if (game1Dat.delete()) System.out.println("Файл \"game1.dat\" удален");
+        if (game2Dat.delete()) System.out.println("Файл \"game2.dat\" удален");
+        if (game3Dat.delete()) System.out.println("Файл \"game3.dat\" удален");
+    }
+
+    private static void saveGame(String path, GameProgress game) {
+        try (FileOutputStream file = new FileOutputStream(path);
+             ObjectOutputStream object = new ObjectOutputStream(file)) {
+            object.writeObject(game);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void zipFiles(String path, List<String> arrayList) {
+        try (ZipOutputStream archive = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String arr : arrayList) {
+                try (FileInputStream fileInpS = new FileInputStream(arr)) {
+                    ZipEntry entry = new ZipEntry(arr);
+                    archive.putNextEntry(entry);
+                    while (fileInpS.available() > 0) {
+                        archive.write(fileInpS.read());
+                    }
+                    archive.closeEntry();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
